381be29d2a945a8fd21d4c09916c495942a6c4cb
David Smith <d.smith@seamsltd.com>
2014-05-08 16:31:48 +0100

    Completed for Project 1

---
+++ plot1.R
@@ -0,0 +1,20 @@
+## Read source data - could be made more efficient by only reading required data
+mydata <- read.csv("household_power_consumption.txt", sep=";")
+
+## Coerce to required data types
+mydata$Time <- strptime(paste(mydata$Date, mydata$Time), format="%d/%m/%Y%H:%M:%S")
+mydata$Date <- as.Date(mydata$Date, format="%d/%m/%Y")
+mydata$Global_active_power <- as.numeric(as.character(mydata$Global_active_power))
+mydata$Global_reactive_power <- as.numeric(as.character(mydata$Global_reactive_power))
+mydata$Sub_metering_1 <- as.numeric(as.character(mydata$Sub_metering_1))
+mydata$Sub_metering_2 <- as.numeric(as.character(mydata$Sub_metering_2))
+mydata$Sub_metering_3 <- as.numeric(as.character(mydata$Sub_metering_3))
+mydata$Voltage <- as.numeric(as.character(mydata$Voltage))
+
+## Subset to required date range
+mydata <- subset(mydata, Date >= as.Date("2007-02-01") & Date <= as.Date("2007-02-02"))
+
+## Plot 1
+png(file="plot1.png", width=480, height=480)
+hist(mydata$Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off()
